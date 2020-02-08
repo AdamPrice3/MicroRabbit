18632dbb4a284aa8c3227f8a45544872cd285b43
adam price <adam.price738@googlemail.com>
2020-02-08 17:28:46 +0000

    Application interface and classes initialised

---
+++ MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Application.Interfaces
+{
+    interface IAccountService
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}

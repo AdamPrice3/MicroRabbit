c97fb4f709fc77d14f4538b6f79e166ddf76c62b
adam price <adam.price738@googlemail.com>
2020-02-08 11:51:28 +0000

    Implementation of RabbitMQ Bus completed

---
+++ MicroRabbit.Domain.Core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core
+{
+    class Class1
+    {
+        static void Main(string[] args) { }
+    }
+}
